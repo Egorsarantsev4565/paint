c0e5657d5096d232a5348229c3519a370035bd31
Егор <egorsarantsev4565@yandex.ru>
2021-01-26 14:13:38 +0300

    Добавьте файлы проекта.

---
+++ paint/Form2.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace paint
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picture.Location = new System.Drawing.Point(0, 0);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(800, 450);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picture);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form2_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+    }
+}
---
+++ paint/Form2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paint
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        } 
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Width = 980;
+            this.Height = 600;
+        }
+
+        private void Form2_Paint(object sender, PaintEventArgs e)
+        {
+           
+
+            Bitmap bmp = new Bitmap(picture.Width, picture.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+           Image dog = Image.FromFile(@"C:\dog.png");
+            graph.FillRectangle(Brushes.Aquamarine, 0, 0, 980, 400);
+            
+            graph.FillRectangle(Brushes.Blue, 450, 200, 250, 200);
+            graph.FillRectangle(Brushes.White, 490, 250, 50, 50);
+            graph.FillRectangle(Brushes.White, 615, 250, 50, 50);
+            graph.FillRectangle(Brushes.Black, 560, 324, 40, 75);
+        
+            PointF point1 = new PointF(450,200);
+            PointF point2 = new PointF(580, 100);
+            PointF point3 = new PointF(700, 200);
+            PointF[] curvePoints = { point1, point2, point3 };
+            graph.FillPolygon(Brushes.Violet, curvePoints);
+            
+            graph.FillRectangle(Brushes.Green, 0, 400, 980 , 200); 
+            graph.DrawImage(dog, 680, 400, 120, 120);
+            graph.FillRectangle(Brushes.Brown, 220, 200, 30, 200);
+            graph.FillEllipse(Brushes.Green, 143, 50, 185, 200);
+           
+            graph.FillEllipse(Brushes.Yellow, 670, 10, 150, 150);
+          
+            graph.FillEllipse(Brushes.White, 750, 10, 130, 100);
+            graph.FillEllipse(Brushes.White, 820, 30, 130, 100);  
+            graph.FillEllipse(Brushes.White, 770, 70, 130, 90);
+            graph.FillEllipse(Brushes.White, 700, 80, 130, 90);
+
+            Font font = new Font("Times New Roman", 20, FontStyle.Bold |
+FontStyle.Italic);
+            Point point = new Point(10, 500);
+            graph.DrawString("Выполнил: Саранцев Егор - 425 группа", font, Brushes.Black, point);
+
+            picture.Image = bmp;
+        }
+
+      
+    }
+}
